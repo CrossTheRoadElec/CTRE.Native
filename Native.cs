ac95f9bc44395c87d569f6f78ce86c6d5211f234
Omar Zrien <ozrien@crosstheroadelectronics.com>
2018-06-22 12:20:48 -0400

    Testing simulation features in VS.

--- Native.cs
+++ Native.cs
@@ -11,12 +11,14 @@ namespace CTRE.Native
 {
     public static class System
     {
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int IoControl(uint ioControlCode, UInt64 ioControlParam, byte[] inValue, byte[] outValue);
+        public static int IoControl(uint ioControlCode, UInt64 ioControlParam, byte[] inValue, byte[] outValue)
+        {
+            return 0;
+        }
     }
     public static class CAN
     {
-		/**
+        /**
 		 * @param arbID CAN Frame Arbitration ID to send.  Set bottom 29 bits or 11 bits depending on arbid type.  
 		 * 													Set BIT31 if using 11bit IDS.
 		 * @param data data bytes to send.  Parameter is little-endian, so the first byte to transmit is the LSB.
@@ -29,9 +31,11 @@ namespace CTRE.Native
 		 *												will match the last value passed into this call.
 		 * @return '0' if successful, negative error code if API failed.
 		 */
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int Send(UInt32 arbId, UInt64 data, UInt32 len, UInt32 periodMs);
-		/**
+        public static int Send(UInt32 arbId, UInt64 data, UInt32 len, UInt32 periodMs)
+        {
+            return 0;
+        }
+        /**
 		 * @param arbID CAN Frame Arbitration ID to receive.  The arbid is in the bottom 29 bits or 11 bits depending on arbid type.  
 		 * 													BIT31 is set if ID is 11bit.
 		 * @param data [out] data bytes to send.  Parameter is little-endian, so the first byte to transmit is the LSB.
@@ -43,15 +47,19 @@ namespace CTRE.Native
 		 * @return '0' if frame was received since last call. '1' if frame was not received since last but params still filled in with latest received values.
 		 *												Negative error code if API failed.
 		 */
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int Receive(UInt32 arbId, ref UInt64 data, ref UInt32 len);
-		/**
+        public static int Receive(UInt32 arbId, ref UInt64 data, ref UInt32 len)
+        {
+            return 0;
+        }
+        /**
 		 * @param arbID CAN Frame Arbitration ID of the buffer to retrieve. BIT31 is set for 11bit IDs. 
 		 * @param data [out] the current bytes being sent for this arbID at the period specified in the last call to Send().
 		 * @return '0' if successful, negative error code if API failed.
-		 */	
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int GetSendBuffer(UInt32 arbId, ref UInt64 data);
+		 */
+        public static int GetSendBuffer(UInt32 arbId, ref UInt64 data)
+        {
+            return 0;
+        }
 
         [StructLayout(LayoutKind.Sequential, Pack = 1)]
         public struct CanSessionMsg
@@ -62,7 +70,7 @@ namespace CTRE.Native
             public UInt32 D4D5D6D7; //!< Last four bytes of data frame.  LSB is the fifth byte sent over the wire.
         }
 
-		/**
+        /**
 		 * Open a buffered stream where CAN frames that pass an arbid Mask/Value pair are buffered into.  
 		 *		The filter checks each frame's ID: (ARBID & MASK) == VALUE, if it passes then it is buffered.
 		 * @param handle [out] handle for newly created stream to use with other functions.
@@ -71,17 +79,21 @@ namespace CTRE.Native
 		 * @return '0' if successful, negative error code if API failed.
 		 */
 
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int OpenStream(ref uint handle, UInt32 arbIdMask, UInt32 arbIdValue);
-		/**
+        public static int OpenStream(ref uint handle, UInt32 arbIdMask, UInt32 arbIdValue)
+        {
+            return 0;
+        }
+        /**
 		 * Gets the number of buffered messages for a given stream.
 		 * @param handle handle for stream.
 		 * @param numMsgs [out] number of messages.
 		 * @return '0' if successful, negative error code if API failed.
 		 */
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int GetStreamSize(uint handle, ref UInt32 numMsgs);
-		/**
+        public static int GetStreamSize(uint handle, ref UInt32 numMsgs)
+        {
+            return 0;
+        }
+        /**
 		 * Pops a buffered messages for a given stream.
 		 * @param handle handle for stream.
 		 * @param arbId [out] The message arbid.
@@ -90,42 +102,64 @@ namespace CTRE.Native
 		 * @param msgsRead [out] unused at time of writing. 
 		 * @return '0' if successful, negative error code if API failed.
 		 */
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int ReadStream(uint handle, ref UInt32 arbId, ref UInt64 data, ref UInt32 len, ref UInt32 msgsRead);
-		/**
+        public static int ReadStream(uint handle, ref UInt32 arbId, ref UInt64 data, ref UInt32 len, ref UInt32 msgsRead)
+        {
+            return 0;
+        }
+        /**
 		 * Closes a stream
 		 * @param handle handle for stream.
 		 */
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static void CloseStream(uint handle);
+        public static void CloseStream(uint handle)
+        {
+        }
     }
     public static class Watchdog
     {
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int Feed(UInt32 timeMs);
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int GetEnableBits(ref UInt32 enabledBits);
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int RegisterSafeOutput(UInt32 portId);
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int RegisterSafePWMOutput(UInt32 PWMId);
+        public static int Feed(UInt32 timeMs)
+        {
+            return 0;
+        }
+        public static int GetEnableBits(ref UInt32 enabledBits)
+        {
+            return 0;
+        }
+        public static int RegisterSafeOutput(UInt32 portId)
+        {
+            return 0;
+        }
+        public static int RegisterSafePWMOutput(UInt32 PWMId)
+        {
+            return 0;
+        }
     }
     public static class HDU
     {
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int GetJoy(ref Int32 updateCnt, Int32[] ints, uint numInts, float[] floats,uint numFloats);
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static void SetMode(UInt32 mode);
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static UInt32 GetMode();
+        public static int GetJoy(ref Int32 updateCnt, Int32[] ints, uint numInts, float[] floats, uint numFloats)
+        {
+            return 0;
+        }
+        public static void SetMode(UInt32 mode)
+        {
+        }
+        public static UInt32 GetMode()
+        {
+            return 0;
+        }
     }
     public static class Misc
     {
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int TimeToProcess();
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static int Bitmap_ConvertBPP(byte[] bimap, byte[] pixelBytes, uint bpp);
-        [MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern public static byte[] GetBitmap(byte[] prevCall, Object obj, uint bpp);
+        public static int TimeToProcess()
+        {
+            return 0;
+        }
+        public static int Bitmap_ConvertBPP(byte[] bimap, byte[] pixelBytes, uint bpp)
+        {
+            return 0;
+        }
+        public static byte[] GetBitmap(byte[] prevCall, Object obj, uint bpp)
+        {
+            return prevCall;
+        }
     }
 }
